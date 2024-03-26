70ea31a0ef4de46447853e6ad3eccc985eed1343
Ivan Shybut <ivanshybut92@gmail.com>
2024-03-26 22:16:02 +0300

    test

---
+++ src/test/java/junit5/api/SimpleApiTest.java
@@ -0,0 +1,15 @@
+package junit5.api;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleApiTest {
+
+    @Test
+    public void getAllUserTest(){
+        given()
+    }
+
+    private void given() {
+    }
+
+}
