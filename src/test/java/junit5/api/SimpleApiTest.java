36597fb0a0cec57b2c6975de031a4cbdae7a5726
Ivan Shybut <ivanshybut92@gmail.com>
2024-03-27 18:08:57 +0300

    fakeapi

--- src/test/java/junit5/api/SimpleApiTest.java
+++ src/test/java/junit5/api/SimpleApiTest.java
@@ -2,14 +2,15 @@ package junit5.api;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class SimpleApiTest {
 
     @Test
     public void getAllUserTest(){
-        given()
+        given().get("https://fakestoreapi.com/users");
     }
 
-    private void given() {
-    }
+
 
 }
