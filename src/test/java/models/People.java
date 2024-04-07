d032c1f6641bc187c2f3e2d4d2480f8131f83d78
Ivan Shybut <ivanshybut92@gmail.com>
2024-04-07 20:07:38 +0300

    finish 024

--- src/test/java/models/People.java
+++ src/test/java/models/People.java
@@ -11,4 +11,27 @@ public class People {
         this.gender = gender;
     }
 
+    @Override
+    public String  toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public Integer getAge(){
+        return age;
+    }
+    public String getGender(){
+        return gender;
+    }
+
+
 }
--- src/test/java/tests/SecParamTest.java
+++ src/test/java/tests/SecParamTest.java
@@ -1,10 +1,31 @@
 package tests;
 
+import models.People;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
 public class SecParamTest {
+
+    private static Stream<Arguments> testPeople(){
+        return Stream.of(
+                Arguments.of(new People("Stas", 18,"male")),
+                Arguments.of(new People("Sasha", 20,"female")),
+                Arguments.of(new People("Misha", 15,"male"))
+
+        );
+    }
     @ParameterizedTest
-    @MethodSource
-    public void
+    @MethodSource("testPeople")
+
+    public void testParamPeople(People people){
+        System.out.println(people.getName() + " " + people.getAge() + " " + people.getGender());
+        Assertions.assertTrue(people.getName().contains("s"));
+
+    }
+
+
 }
