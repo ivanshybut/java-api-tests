67ace399da6496df540e77105dc4d3e5f337595f
Ivan Shybut <ivanshybut92@gmail.com>
2024-04-10 13:27:17 +0300

    025

---
+++ src/test/java/tests/JacksonTest025.java
@@ -0,0 +1,12 @@
+package tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+public class JacksonTest025 {
+    @Test
+    public void jacksonTest(){
+        ObjectMapper objectMapper = new ObjectMapper();
+         
+    }
+}
