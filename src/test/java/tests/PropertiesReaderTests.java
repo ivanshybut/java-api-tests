1eee7374b3c1813c2c7f9f3ab036314b1d940026
Ivan Shybut <ivanshybut92@gmail.com>
2024-04-19 21:48:23 +0300

    026 01

--- src/test/java/tests/PropertiesReaderTests.java
+++ src/test/java/tests/PropertiesReaderTests.java
@@ -13,9 +13,16 @@ public class PropertiesReaderTests {
     @SneakyThrows
     public void simpleReaderTest() throws IOException {
         Properties properties = new Properties();
-        FileInputStream fs = new FileInputStream("src/tests/resources/project.properties");
+        FileInputStream fs = new FileInputStream("/Users/ivanshybut/java-api-tests/JavaApiTests/src/test/resources/project.properties");
         properties.load(fs);
 
+        String url = properties.getProperty("url");
+        boolean isProduction = Boolean.parseBoolean(properties.getProperty("is_production"));
+        int threads = Integer.parseInt(properties.getProperty("threads"));
+
+        System.out.println(url);
+        System.out.println(isProduction);
+        System.out.println(threads);
 
     }
 }
