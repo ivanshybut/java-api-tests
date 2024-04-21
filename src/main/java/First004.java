8697f3cffe4747ff52f44066e5bf991a37a529f6
Ivan Shybut <ivanshybut92@gmail.com>
2024-04-21 21:23:27 +0300

    start 004 java

---
+++ src/main/java/First004.java
@@ -0,0 +1,10 @@
+public class First004 {
+    public static void main(String[] args) {
+        int age = 20;
+        double age2 = 10.5;
+        String name = "Ivan";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(name);
+    }
+}
